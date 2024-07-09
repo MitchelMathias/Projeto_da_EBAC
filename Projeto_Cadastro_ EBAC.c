b732b5b000a0c72547917b05a29324359dcb5327
Mitchel Mathias <mitchelmathias2904@gmail.com>
2024-07-09 19:43:06 -0300

    09/07/2024

--- Projeto_Cadastro_ EBAC.c
+++ Projeto_Cadastro_ EBAC.c
@@ -4,12 +4,36 @@
 
 int main()
 {
-	setlocale(LC_ALL, "Portuguese");
-	printf("========== Cartório EBAC ==========\n");
-	printf("Escolha uma das opções abaixo:\n\n");
+	int opcao = 0; // Declarando a variável opção
+	
+	setlocale(LC_ALL, "Portuguese"); // definindo o idioma (sinais, ç )
+	
+	printf("========== Cartório EBAC ==========\n"); // Cabeçalho do projeto pro usuario 
+	printf("Escolha uma das opções abaixo:\n\n"); // Inicio das oções
 	printf("\t1 - Consultar Usuário\n");
 	printf("\t2 - Cadastrar Usuário\n");
-	printf("\t3 - Deletar Usuário\n");
+	printf("\t3 - Deletar Usuário\n\n"); // fim das opções
+	printf("Qual opção: "); // Mensagem pro usuário identificar que é aqui que ele vai digitar a sua escolha
 	
-	printf("\n\nProjeto EBAC - Mitchel Mathias");
+	scanf("%d", &opcao); // Ler a escolha do usuário
+	
+	system("cls"); // limpa a tela para o usuário
+	
+	if (opcao ==  1){ // Aqui usa a estrutura "se" para realizar uma instrução apenas se a função scanf ler o número 1
+		printf("Você escolheu Consultar um Usuário!!\n\n");
+	}
+	if (opcao == 2){ // Aqui usa a estrutura "se" para realizar uma instrução apenas se a função scanf ler o número 2
+		printf("Você escolheu Cadastrar um Usuário!!\n\n");
+	}
+	if (opcao == 3){ // Aqui usa a estrutura "se" para realizar uma instrução apenas se a função scanf ler o número 2
+		printf("Você escolheu Deletar um Usuário!!\n\n");
+	}
+	if (opcao > 3) { // Aqui usa a estrutura "se" para caso o usuário digite qualquer número acima de 1, 2 e 3
+		printf("Essa opção é inválida!\n\n");
+	}
+	if (opcao < 1) { // Aqui usa a estrutura "se" para caso o usuário digite qualquer número abaixo de 1, 2 e 3
+		printf("Essa opção é inválida!\n\n");
+	}
+	printf("\n\nProjeto EBAC - Mitchel Mathias"); // Mensagem informando o nome do projeto e quem desenvolveu
 }
+	
