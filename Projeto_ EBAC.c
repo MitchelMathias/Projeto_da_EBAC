00ad0468ebd1bc27f5651a89d6a5bd7b052ccc85
Mitchel Mathias <mitchelmathias2904@gmail.com>
2024-07-29 19:10:03 -0300

    29/07/2024
    Correção de bugs e acrescentando comentários...

--- Projeto_ EBAC.c
+++ Projeto_ EBAC.c
@@ -5,20 +5,19 @@
 
 int consulta() { // Função consulta
     setlocale(LC_ALL, "portuguese"); // Definindo o Idioma
+    
+    char cpf[12], nome[15], sobrenome[15], cargo[30], conteudo[72], cpf_busca[12]; // Declarando Variáveis
    
-    char repeticao [2];
-    do {
-    	printf("Você escolheu Consultar um usuário!!\n\n");
-    	
-	    char cpf[12], nome[15], sobrenome[15], cargo[30], conteudo[72], cpf_busca[12]; // Declarando Variáveis
-	
-	    printf("Digite o CPF: "); // Mensagem para o usuário 
+    char repeticao [2]; // Declarando variável
+    do { // faça enquanto a condição for verdadeira
+    	printf("Você escolheu Consultar um usuário!!\n\n"); // Mensagem pro usuário 
+		printf("Digite o CPF: "); // Mensagem para o usuário 
 	    scanf("%s", cpf_busca); // Guarda a entrada na variável cpf_busca
 	
 	    FILE *arquivo; // Acesse a estrutura FILE e atribua o caminho na variável arquivo
 	    arquivo = fopen("cadastros.txt", "r"); // abra o arquivo cadastros em txt e leia
 	    if (arquivo == NULL) { // se arquivo não encontrado 
-	    	printf("Nenhum usuário cadastrado...\n");
+	    	printf("Nenhum usuário cadastrado...\n"); // Mensagem pro usuário
 	    }
 	    
 	    int cpf_encontrado = 0; // declarando variável para corrigir o erro de repetição de usuário não cadastrado
@@ -27,27 +26,27 @@ int consulta() { // Função consulta
 	        sscanf(conteudo, "%s - %s - %s - %s", cpf, nome, sobrenome, cargo); // Extrai dados formatados da string 'conteudo' e os armazena nas variáveis 'cpf', 'nome', 'sobrenome' e 'cargo'
 	
 	        if (strcmp(cpf, cpf_busca) == 0) { // Compare cpf com cpf_busca e se o retorno da FUNÇÃO for == 0 que quer dizer exatamente iguais faça os comandos abaixo 
-	            printf("\nEssas são as informações do usuário!!\n\n");
+	            printf("\nEssas são as informações do usuário!!\n\n"); // Mensagem pro usuário
 	            printf("\nNome: %s %s\n", nome, sobrenome); // Escreva Nome e depois os conteúdos nome e sobrenome separados por um " "
 	            printf("Cpf: %s\n", cpf); // Escreva Cpf e depois o dado guardado em cpf 
 	            printf("Cargo: %s\n\n", cargo); // Escreva Cargo e depois o dado guardado em cargo 
-	            cpf_encontrado = 1;
-	            break;
+	            cpf_encontrado = 1; // se tiver um cpf identico ao cpf busca variável recebe o valor de 1
+	            break;// pare
 	        }
 	    }
 	
 	    if (cpf_encontrado == 0) { // Corrigindo o erro de repetição de usuário não cadastrado
 	        printf("\nUsuário não cadastrado!!...\n\n"); // Caso não encontrou o CPF
 	    }
-	    fclose(arquivo);
-	    do {
-		    printf("Quer consultar mais 1 usuário? [S/N] --> ");
-		    scanf("%s", repeticao );
-		    printf("\n");
-		} while(repeticao[0] != 'S' && repeticao[0] != 's' && repeticao[0] != 'N' && repeticao[0] != 'n');
+	    fclose(arquivo); // fecha o arquivo
+	    do { // faça abaixo enquanto a condição for diferente
+		    printf("Quer consultar mais 1 usuário? [S/N] --> "); // Mensagem pro usuário
+		    scanf("%s", repeticao ); // salve dado tipo string na variável repetição
+		    printf("\n");// pula uma linhda
+		} while(repeticao[0] != 'S' && repeticao[0] != 's' && repeticao[0] != 'N' && repeticao[0] != 'n'); // faça enquanto a condição é diferente
 	
-		system("cls");
-	}while (repeticao[0] == 'S' || repeticao[0] == 's' );
+		system("cls"); // Limpa a tela
+	}while (repeticao[0] == 'S' || repeticao[0] == 's' ); // enquanto a condição for igual a S repita
 	return 0;
 }
 
@@ -56,12 +55,13 @@ int cadastro() { // Função cadastro
 
     printf("Você escolheu Cadastrar usuário!!\n\n"); // Mensagem para o usuário
 	
-	char repeticao [2];
-	int continua_repeticao = 1;
+	char repeticao [2]; // Declarando variável 
+	int continua_repeticao = 1; // Declarando variável e atribuindo o valor de 1
+	char cpf[12], nome[15], sobrenome[15], cargo[30], cpf_busca[12], conteudo[72]; // Declarando Variáveis tipo string caracter
+	int opcao = 1, cpf_encontrado = 0; // Declarando Variáveis tipo inteiro
 		
-	do {
-	    char cpf[12], nome[15], sobrenome[15], cargo[30], cpf_busca[12], conteudo[72]; // Declarando Variáveis tipo string caracter
-	    int opcao = 1, cpf_encontrado = 0; // Declarando Variáveis tipo inteiro
+	do { // faça enquanto a condição por verdadeira
+	    cpf_encontrado = 0;
 	    
 	    FILE *arquivo; // Acesse a estrutura FILE e atribua o caminho na variável arquivo
 	    arquivo = fopen("cadastros.txt", "r"); // abra o arquivo do caminho arquivo e atualiza ou cria o arquivo
@@ -74,44 +74,44 @@ int cadastro() { // Função cadastro
 	    printf("Digite o CPF: "); // Mensagem para o usuário
 	    scanf("%s", cpf); // Guardando o dado na variável CPF 
 	    
-	    strcpy(cpf_busca, cpf);
+	    strcpy(cpf_busca, cpf); // copia o dado da variável cpf_busca pra cpf
 	    
-	    while(fgets(conteudo, sizeof(conteudo), arquivo) != NULL){
-	    	sscanf(conteudo, "%s - %s - %s -%s", cpf, nome, sobrenome, cargo);
+	    while(fgets(conteudo, sizeof(conteudo), arquivo) != NULL){ // Enquanto houver linhas para ler no arquivo (fgets retorna um ponteiro não nulo) lê a linha do arquivo e armazena no (buffer == armazenamento temporário) 'conteudo' com tamanho máximo especificado.
+	    	sscanf(conteudo, "%s - %s - %s -%s", cpf, nome, sobrenome, cargo); // Separa e armzena o conteudo do buffer em variáveis 
 	    	
-	    	if (strcmp(cpf,cpf_busca) == 0){
-	    		printf("\nja tem um usuário cadastrado com esse CPF!!");
-	    		cpf_encontrado += 1;
-	    		break;
+	    	if (strcmp(cpf,cpf_busca) == 0){ // Se o retorno da função strcmp() for == 0 quer dizer que é exatamente iguais
+	    		printf("\nja tem um usuário cadastrado com esse CPF!!"); // Mensagem pro usuário
+	    		cpf_encontrado += 1; // Variável recebe ela mesmo +1
+	    		break; // Sai do loop
 			}
 		}
-		fclose(arquivo);
+		fclose(arquivo); // fecha o arquivo
 		
-		if (cpf_encontrado >= 1){
-			printf("\n\nEscolha uma das opções abaixo:\n\n");
-            printf("\t1 - Deletar registro\n");
-            printf("\t2 - Cancelar\n");
-            printf("\t\nEscolha uma opção -->  ");
-            scanf("%d", &opcao);
+		if (cpf_encontrado >= 1){ // Se cpf_encontrador for maior que 1 faça
+			printf("\n\nEscolha uma das opções abaixo:\n\n"); // Cabeçalho do menu
+            printf("\t1 - Deletar registro\n"); // Opção 1
+            printf("\t2 - Cancelar\n"); // Opção 2
+            printf("\t\nEscolha uma opção -->  ");// mensagem pro usuário
+            scanf("%d", &opcao); // salva tipo inteiro na variável opção
             
-            switch(opcao){
-            	case 1:
-            		system("cls");
-            		deletar();
+            switch(opcao){ // Ifs e else 
+            	case 1: // opção 1
+            		system("cls"); // limpa tela
+            		deletar(); // chama a função deletar
+            		break; 
+            	case 2: // Opção 2
+            		system("cls"); // limpa a tela e não faz nada demais 
             		break;
-            	case 2:
-            		system("cls");
-            		break;
-            	default:
-            		system("cls");
-            		printf("Opção inválida!!!\n");
+            	default: // caso contrario 
+            		system("cls"); // limpa tela
+            		printf("Opção inválida!!!\n");// escreve pro usuário
             		break;
 			}
 		}
-		else{
-			arquivo = fopen("cadastros.txt", "a");
-			if(arquivo == NULL){
-				printf("Erro ao abrir o arquivo!!\n");
+		else{ // Se cpf encontrado for menor que 1 
+			arquivo = fopen("cadastros.txt", "a"); // Abre o arquivo
+			if(arquivo == NULL){ // Se não existir o arquivo 
+				printf("Erro ao abrir o arquivo!!\n"); // escreva a mensagem
 			}
 			
 			printf("Nome: "); // Mensagem para o usuário
@@ -123,23 +123,23 @@ int cadastro() { // Função cadastro
 		    printf("Cargo: "); // Mensagem para o usuário
 		    scanf("%s", cargo); // Guardando o dado na variável cargo 
 		    
-		    fprintf(arquivo, "%s - %s - %s - %s\n", cpf_busca, nome, sobrenome, cargo);
-		    fclose(arquivo);
+		    fprintf(arquivo, "%s - %s - %s - %s\n", cpf_busca, nome, sobrenome, cargo); // Escreva no arquivo de maneira formatada
+		    fclose(arquivo); // Fecha o arquivo
 		    
-		    printf("\nUsuário cadastrado com sucesso!!");
+		    printf("\nUsuário cadastrado com sucesso!!"); // Escreve a mensagem pro usuário 
 			
-			do {
-			    printf("\nQuer cadastrar mais 1 usuário? [S/N] --> ");
-			    scanf("%s", repeticao );
+			do {// Faça enquanto a condição for verdadeira
+			    printf("\nQuer cadastrar mais 1 usuário? [S/N] --> "); // Mensagem pro usuário
+			    scanf("%s", repeticao ); // Salva como string na variável repetição
 			    printf("\n");
-			} while(repeticao[0] != 'S' && repeticao[0] != 's' && repeticao[0] != 'N' && repeticao[0] != 'n');
+			} while(repeticao[0] != 'S' && repeticao[0] != 's' && repeticao[0] != 'N' && repeticao[0] != 'n'); // Enquanto diferente de 'SsNn'
 			
-			if (repeticao[0] != 'S' && repeticao[0] != 's'){
-				continua_repeticao = 0;
+			if (repeticao[0] != 'S' && repeticao[0] != 's'){ // se repetição na posição zero for diferente de 'Ss'
+				continua_repeticao = 0; // continua repetição recebe o valor de zero
 			}	
 		}
-		system("cls");
-	}while (continua_repeticao == 1 );
+		system("cls"); // Limpa tela
+	}while (continua_repeticao == 1 ); // enquanto continua_repeticao for == 1
 	return 0;
 }
 
@@ -147,10 +147,9 @@ int deletar() { // Função deletar
     setlocale(LC_ALL, "portuguese"); // Definindo idioma
 	
 	char repeticao[2];
+	char cpf[12], nome[15], sobrenome[15], cargo[30], conteudo[72], cpf_busca[12]; // Declarando Variáveis
 	
 	do {
-	    char cpf[12], nome[15], sobrenome[15], cargo[30], conteudo[72], cpf_busca[12]; // Declarando Variáveis
-	
 	    printf("Digite o CPF do usuário a ser deletado: "); // Mensagem para o usuário
 	    scanf("%s", cpf_busca); // Guardando o valor na variável cpf_busca 
 	
@@ -196,12 +195,12 @@ int deletar() { // Função deletar
 	    remove("cadastros.txt"); // Apaga o arquivo cadastros.txt
 	    rename("temp.txt", "cadastros.txt"); // Renomeia o arquivo temp.txt para cadastros.txt
 	    
-		printf("Quer deletar mais Algum usuário? [S/N]--> ");
-		scanf("%s", repeticao);
+		printf("Quer deletar mais Algum usuário? [S/N]--> "); // Mensagem pro usuário
+		scanf("%s", repeticao); // Salva como string na variável repetição
 		
-		system("cls");
+		system("cls"); // Limpa a tela
 	    
-	} while (repeticao[0] == 's' || repeticao[0] == 'S');
+	} while (repeticao[0] == 's' || repeticao[0] == 'S'); // Equanto for diferente de 'Ss'
 }
 
 int main() { // Função principal onde puxa as outras funções
